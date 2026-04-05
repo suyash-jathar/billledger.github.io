49bd365b932017d97fc040336ee20398e80e7adc
suyash-jathar <suyashjathar15@gmail.com>
2026-04-05 16:09:21 +0530

    Updating website

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1534377921"
+    serviceWorkerVersion: "2955018080"
   }
 });
