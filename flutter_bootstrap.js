3b15a5218f349261334bb6612b5127466fc4288b
suyash-jathar <suyashjathar15@gmail.com>
2026-08-08 13:46:52 +0530

    Deploy latest build updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2238909918" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "210356568" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
